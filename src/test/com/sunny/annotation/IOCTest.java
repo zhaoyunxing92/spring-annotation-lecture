38b9b89d14b9c8861f61406ed1ca4e4c66082c0b
zhaoyunxing92 <2385585770@qq.com>
2018-11-30 01:58:05 +0800

    添加junit

---
+++ src/test/com/sunny/annotation/IOCTest.java
@@ -0,0 +1,18 @@
+package com.sunny.annotation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author zhaoyunxing92
+ * @date: 2018-11-30 01:42
+ * @des:
+ */
+public class IOCTest {
+
+    @Test
+    public void person() {
+        System.out.println(555);
+    }
+}

b71457ff08e19c3e8bd0f53d4814cf02934f762e
zhaoyunxing92 <2385585770@qq.com>
2018-12-17 02:23:24 +0800

    添加源码位置

--- src/main/java/com/sunny/aware/BeanAware.java
+++ src/main/java/com/sunny/aware/BeanAware.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Component;
 /**
  * @author zhaoyunxing92
  * @date: 2018-12-17 02:02
- * @des:
+ * @des: 获取bean工厂
+ * @see org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#initializeBean
+ * @see org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#invokeAwareMethods
  */
 @Component
 public class BeanAware implements BeanFactoryAware {
